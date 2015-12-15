c5b318bd2ec9acf8230b24c40717343e9788f518
w572722615 <572722615@qq.com>
2015-12-15 22:02:47 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/customview/PhoneEdit.java
@@ -0,0 +1,106 @@
+package com.example.administrator.customview;
+
+/**
+ * Created by Administrator on 2015/12/15.
+ */
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+
+/**
+ * Created by Administrator on 2015/10/7.
+ */
+public class PhoneEdit extends RelativeLayout implements View.OnClickListener{
+    private Context mContext;
+    private EditText phoneEdit;
+    private ImageView clearInput;
+    public PhoneEdit(Context context) {
+        super(context);
+        initData(context);
+    }
+
+    public String getPhoneString(){
+        return phoneEdit.getText().toString().trim();
+    }
+
+    public PhoneEdit(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initData(context);
+    }
+
+    private void initData(Context context) {
+        mContext = context;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.phone_edittext_layout,this,true);
+
+        phoneEdit = (EditText) findViewById(R.id.phoneEdit);
+        clearInput = (ImageView) findViewById(R.id.clearInput);
+        phoneEdit.addTextChangedListener(new MyTextWatcher());
+        clearInput.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+
+            case R.id.clearInput:
+                phoneEdit.setText("");
+                break;
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+        int length ;
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!TextUtils.isEmpty(s)) {
+                length = s.length();
+                if(length>0){
+                    clearInput.setVisibility(VISIBLE);
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            length = s.length();
+            if(length==0){
+                clearInput.setVisibility(GONE);
+            }
+        }
+    }
+
+    public EditText getPhoneEdit(){
+        return phoneEdit;
+    }
+
+    public void changeTypeText(){
+        phoneEdit.setFilters(new InputFilter[]{new InputFilter.LengthFilter(20)});
+        phoneEdit.setInputType(InputType.TYPE_CLASS_TEXT);
+
+    }
+
+    public void changeHintName(){
+        phoneEdit.setHint(getResources().getString(R.string.name));
+    }
+
+
+}
